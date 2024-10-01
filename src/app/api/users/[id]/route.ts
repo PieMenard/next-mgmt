6b7bdbf5d5c0de22d7b83cf0438c52b039cf955d
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-30 22:28:10 -0400

    add get user by id request

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from '@/utils/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const id = parseInt(req.url.split('/users/')[1]);
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      return NextResponse.json(
+        { success: false, error: 'User not found' },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ success: true, data: user });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
+}
