8caba3909690bbf364c8338cf071ac4fc47d3852
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-30 22:19:07 -0400

    add create user request

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/utils/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const user = await prisma.user.create({
+      data: {
+        userName: data.userName,
+        token: data.token,
+      },
+    });
+    return NextResponse.json({ success: true, data: user });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
+}
