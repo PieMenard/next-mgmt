f0531aa6506bd81ffb818ca2f8b82319978a41af
PieMenard <pinedo.m.gustavo@gmail.com>
2024-10-01 00:03:26 -0400

    add request to update a project

--- src/app/api/users/[userId]/projects/[projectId]/route.ts
+++ src/app/api/users/[userId]/projects/[projectId]/route.ts
@@ -23,3 +23,31 @@ export async function GET(
     return NextResponse.json({ success: false, error: error });
   }
 }
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { userId: string; projectId: string } }
+) {
+  try {
+    const data = await req.json();
+    const project = await prisma.project.update({
+      where: {
+        id: parseInt(params.projectId),
+        userId: parseInt(params.userId),
+      },
+      data: {
+        name: data.name,
+        unit: data.unit,
+      },
+    });
+    if (!project) {
+      return NextResponse.json(
+        { success: false, error: 'Project not found' },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ success: true, data: project });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
+}
