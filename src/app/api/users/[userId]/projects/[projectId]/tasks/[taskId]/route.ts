b022a4e939c3bfda8f6fea819ce187771cb811a6
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-10-01 15:56:50 -0400

    add request to update a task

--- src/app/api/users/[userId]/projects/[projectId]/tasks/[taskId]/route.ts
+++ src/app/api/users/[userId]/projects/[projectId]/tasks/[taskId]/route.ts
@@ -5,12 +5,45 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     req: NextRequest,
-    { params }: { params: { projectId: string } }
+    { params }: { params: { projectId: string, taskId: string } }
 ) {
     try {
         const task = await prisma.task.findUnique({
             where: {
-                id: parseInt(params.projectId),
+                projectId: parseInt(params.projectId),
+                id: parseInt(params.taskId),
+            },
+        });
+        if (!task) {
+            return NextResponse.json(
+                { success: false, error: 'Task not found' },
+                { status: 404 }
+            );
+        }
+        return NextResponse.json({ success: true, data: task });
+    } catch (error) {
+        return NextResponse.json({ success: false, error: error });
+    }
+}
+
+
+export async function PUT(
+    req: NextRequest,
+    { params }: { params: { projectId: string, taskId: string } }
+) {
+    try {
+        const data = await req.json();
+        const task = await prisma.task.update({
+            where: {
+                id: parseInt(params.taskId),
+                projectId: parseInt(params.projectId),
+            },
+            data: {
+                name: data.name,
+                complexityLevel: data.complexityLevel,
+                priority: data.priority,
+                description: data.description,
+                quantity: data.quantity,
             },
         });
         if (!task) {
