7f9d58cfac59ffd6b842a7ca42a97f7c88ff7d33
PieMenard <pinedo.m.gustavo@gmail.com>
2024-10-02 19:49:54 -0400

    add 2 query parameters that allows ordering the projects:

--- src/app/api/users/[userId]/projects/[projectId]/tasks/route.ts
+++ src/app/api/users/[userId]/projects/[projectId]/tasks/route.ts
@@ -1,69 +1,78 @@
 //users/[userId]/projects/[projectId]/tasks
 
-import { prisma } from "@/utils/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { prisma } from '@/utils/prisma';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(
-    req: NextRequest,
-    { params }: { params: { userId: string, projectId: string } }
+  req: NextRequest,
+  { params }: { params: { userId: string; projectId: string } }
 ) {
-    const projectId = parseInt(params?.projectId);
+  const projectId = parseInt(params?.projectId);
 
-    try {
-        const data = await req.json();
-        const task = await prisma.task.create({
-            data: {
-                name: data.name,
-                dateOfRecording: (new Date()).toISOString().slice(0, 10).replace(/-/g, ""),
-                complexityLevel: data.complexityLevel,
-                priority: data.priority,
-                description: data.description,
-                quantity: data.quantity,
-                project: {
-                    connect: {
-                        id: projectId,
-                    },
-                },
-            },
-        });
-        return NextResponse.json({ success: true, data: task });
-    } catch (error) {
-        return NextResponse.json({ success: false, error: error });
-    }
+  try {
+    const data = await req.json();
+    const task = await prisma.task.create({
+      data: {
+        name: data.name,
+        dateOfRecording: new Date()
+          .toISOString()
+          .slice(0, 10)
+          .replace(/-/g, ''),
+        complexityLevel: data.complexityLevel,
+        priority: data.priority,
+        description: data.description,
+        quantity: data.quantity,
+        project: {
+          connect: {
+            id: projectId,
+          },
+        },
+      },
+    });
+    return NextResponse.json({ success: true, data: task });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
 }
 
-export async function GET(req: NextRequest, { params }: { params: { projectId: string } }) {
-    const projectId = params?.projectId
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { projectId: string } }
+) {
+  const projectId = params?.projectId;
 
-    const { searchParams } = new URL(req.url);
+  const { searchParams } = new URL(req.url);
 
-    const complexityFilter = searchParams.get('complexity')
-    const priorityFilter = searchParams.get('priority')
+  const complexityFilter = searchParams.get('complexity');
+  const priorityFilter = searchParams.get('priority');
+  const sort_by = searchParams.get('sort_by') || 'dateOfRecording';
+  const order_by = searchParams.get('order_by') || 'asc';
 
-    const filters: any = {}
+  const filters: any = {};
 
-    if (projectId) {
-        filters.projectId = parseInt(projectId)
+  if (projectId) {
+    filters.projectId = parseInt(projectId);
+  }
+  if (complexityFilter) {
+    filters.complexityLevel = parseInt(complexityFilter);
+  }
+  if (priorityFilter) {
+    filters.priority = parseInt(priorityFilter);
+  }
+
+  try {
+    const data = await prisma.task.findMany({
+      where: filters,
+      orderBy: { [sort_by]: order_by },
+    });
+    if (data.length === 0) {
+      return NextResponse.json(
+        { success: false, error: 'No tasks found' },
+        { status: 404 }
+      );
     }
-    if (complexityFilter) {
-        filters.complexityLevel = parseInt(complexityFilter)
-    }
-    if (priorityFilter) {
-        filters.priority = parseInt(priorityFilter)
-    }
-
-    try {
-        const data = await prisma.task.findMany({
-            where:
-                filters
-
-        })
-        if (data.length === 0) {
-            return NextResponse.json({ success: false, error: 'No tasks found' }, { status: 404 })
-        }
-        return NextResponse.json({ success: true, data: data })
-    } catch (error) {
-        return NextResponse.json({ success: false, error: error });
-    }
-
+    return NextResponse.json({ success: true, data: data });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
 }
